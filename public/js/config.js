f772fd18345ee25689923779e2253dba44e6bd39
pratikonly <ashishkumarsinghx@gmail.com>
2025-10-03 22:11:39 +0530

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,9 @@ export const API_BASE = window.location.origin;
 
 export const ENDPOINTS = {
   AUTH: `${API_BASE}/api/auth`,
-  MESSAGES: `${API_BASE}/api/messages`
+  MESSAGES: `${API_BASE}/api/messages`,
+  USERS: `${API_BASE}/api/users`
 };
 
 export const POLL_INTERVAL = 2000; // Poll for new messages every 2 seconds
+export const USERS_POLL_INTERVAL = 5000; // Poll for online users every 5 seconds
